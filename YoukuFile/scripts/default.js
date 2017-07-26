35b552214a27b3eed3ba68a5fd9ea7dd245fb5ed
ChangerHe <244737162@qq.com>
2017-07-26 21:48:36 +0800

    YouKu file change

---
+++ YoukuFile/scripts/default.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+	var nav = document.getElementById("j-nav");
+	var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+	var imgCon = document.getElementsByClassName("imgCon");
+	
+}
