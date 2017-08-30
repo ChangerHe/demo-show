20585a142c2f2b1d465c617ab1804f6aaf505bbd
ChangerHe <244737162@qq.com>
2017-08-30 18:03:37 +0800

    抽取的部分

--- OnlineStoreProject/scripts/index.js
+++ OnlineStoreProject/scripts/index.js
@@ -1,49 +1,3 @@
-// $('#item >li').hover(function() {
-//     $(this).addClass('hoverLi')
-//     $(this).find('.content').show()
-// }, function() {
-//     $(this).removeClass('hoverLi')
-//     $(this).find('.content').hide()
-// })
-
-// 实现无延迟高容差的菜单效果
-
-// 定义激活的li的jQuery效果
-function activeAni(tag) {
-    $(tag).addClass('hoverLi')
-    $(tag).find('.content').show()
-}
-
-// 定义移除的li的jQuery效果
-function reactiveAni(tag) {
-    $(tag).removeClass('hoverLi')
-    $(tag).find('.content').hide()
-}
-
-// 将方法写入插件,执行
-$('#item').menuAim({
-    activate: activeAni, // fired on row activation
-    deactivate: reactiveAni, // fired on row deactivation
-    exitMenu: function() { return true; }
-});
-
-// 顶部最大的轮播图效果
-$("#slideBoxSmall").tyslide({
-    boxh: 430, //盒子的高度
-    w: 1000, //盒子的宽度
-    h: 400, //图片的高度
-    isShow: true, //是否显示控制器
-    isShowBtn: true, //是否显示左右按钮
-    controltop: 10, //控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
-    controlsW: 16, //控制按钮宽度
-    controlsH: 16, //控制按钮高度
-    radius: 8, //控制按钮圆角度数
-    controlsColor: "#d8d8d8", //普通控制按钮的颜色
-    controlsCurrentColor: "#ff6600", //当前控制按钮的颜色
-    isShowNumber: true,
-    marginNum: 2
-});
-
 // 整个页面的第二个轮播图,也就是最小到的那个轮播图的效果
 $("#sevenCountBanner,#sevenCountBanner1,#sevenCountBanner2").tyslide({
     boxh: 216, //盒子的高度
@@ -79,13 +33,6 @@ $("#ruibikaSell,#outDoorBanner,#childrensBanner").tyslide({
 });
 
 
-// 为文中的样式及文字提供默认显示效果,对应接上了相应的接口
-$(window).pub({
-    searchItem: '想啥就啥', // 提供搜索栏的默认显示值
-    shopCarNum: 5 // 提供购物车的数量
-})
-
-
 
 // 添加相应的hover效果,为标题右边的三栏菜单
 function hoverAnimate(fatherTag) {
@@ -106,26 +53,6 @@ hoverAnimate('.outDoors')
 hoverAnimate('.childClothes')
 hoverAnimate('.popularize')
 
-// 为新书畅销榜,从你的全世界路过部分,添加相应的hover样式
-
-$('.wellSell').hover(function() {
-    // 因为出现了没有固定块的高度而导致每个书籍下方的横线出现变短的情况,因此在这里将每个盒子的高度固定
-    $('.wellSell').css('height', '42px')
-        // 将所有wellSell中的wellBook类隐藏
-    $('.wellSell').find('.wellBook').slideUp('fast')
-        // 将所有wellSell中的anitherText类隐藏
-    $('.wellSell').find('.anotherText').slideUp('fast')
-        // 将当前wellSell中的wellBook类显现
-    $(this).find('.wellBook').stop(true, false).slideDown('fast')
-        // 将当前wellSell中的anitherText类显现
-    $(this).find('.anotherText').stop(true, false).slideDown('fast')
-        // 设置当前wellSell的高度
-    $(this).css('height', '148px')
-}, function() {
-    // $(this).find('.wellBook').stop(true, true).slideUp('fast')
-    // $(this).find('.anotherText').stop(true, true).slideUp('fast')
-    // $(this).css('height', '42px')
-})
 
 // 为奶嘴部分添加hover效果
 
@@ -145,63 +72,4 @@ $(window).on('scroll', function() {
         $('.searchFxedBar').slideUp('slow')
         $('.leftFloor').hide()
     }
-})
-
-
-// 为左侧楼层增加相应的效果
-$('.leftFloor li').hover(function() {
-    // 为鼠标移入的span标签添加active
-    $(this).addClass('active')
-        // 定义一个leftFloorCurr,存储背景定位的X轴位置
-    var leftFloorCurr = parseInt($('.leftFloor .active').css('backgroundPositionX'))
-        // X轴位置交给本身
-    $(this).css('backgroundPositionX', leftFloorCurr - 40 + 'px')
-        // 为右侧内容设置滑动上去之后显示的效果
-    $(this).stop(true, false).animate({
-        width: '80px'
-    }, 100)
-}, function() {
-    //定义一个leftFloorCurr,存储背景定位的X轴位置
-    var leftFloorCurr = parseInt($('.leftFloor .active').css('backgroundPositionX'))
-        // X轴位置交给本身
-    $(this).css('backgroundPositionX', leftFloorCurr + 40 + 'px')
-        // 移除身上的active
-    $('.leftFloor li').removeClass('active')
-        // 为右侧内容设置滑动上去之后隐藏的效果
-    $(this).stop(true, true).animate({
-        width: '40px'
-    }, 100)
-})
-
-// 定义楼层的滚动效果
-$('.leftFloor li').on('click', function() {
-    var a = 1100;
-    $('html,body').animate({
-        scrollTop: a + $(this).index() * 610
-    }, 1000)
-})
-
-
-// 定义右侧楼策的效果,
-$('.rightFloor .discount').on('click', function() {
-    // 点击时,右侧的span移除
-    $(this).find('span').stop(true, false).animate({
-        right: '40px'
-    })
-}).on('mouseout', function() {
-    // 鼠标移出,则右侧的二维码隐藏
-    $(this).find('span').animate({
-        right: '-60px'
-    })
-})
-
-// 为二维码增加hover则暂时不隐藏的效果,并解决了动画一直在播放的bug
-$('.rightFloor .discount span').hover(function() {
-    $(this).stop(true, false).animate({
-        right: '40px'
-    })
-}, function() {
-    $(this).stop(true, true).animate({
-        right: '-60px'
-    })
 })
--- OnlineStoreProject/scripts/public.js
+++ OnlineStoreProject/scripts/public.js
@@ -6,6 +6,9 @@
  *      
  */
 
+
+
+// 以下是自己封装的插件部分
 ;
 (function($) {
     $.fn.extend({
@@ -38,7 +41,13 @@
 })(jQuery);
 
 
-// 以下是无延迟高容差的菜单效果
+// 为文中的样式及文字提供默认显示效果,对应接上了相应的接口,插件在上面
+$(window).pub({
+    searchItem: '想啥就啥', // 提供搜索栏的默认显示值
+    shopCarNum: 5 // 提供购物车的数量
+})
+
+// 以下是无延迟高容差的菜单效果的封装代码插件部分
 ;
 (function($) {
     $.fn.menuAim = function(opts) {
@@ -193,3 +202,121 @@
     };
 })(jQuery);
 // 结束,无延迟高容错导航菜单的封装源码,来自jquery-menu-aim
+
+// 实现无延迟高容差的菜单效果
+
+// 定义激活的li的jQuery效果
+function activeAni(tag) {
+    $(tag).addClass('hoverLi')
+    $(tag).find('.content').show()
+}
+
+// 定义移除的li的jQuery效果
+function reactiveAni(tag) {
+    $(tag).removeClass('hoverLi')
+    $(tag).find('.content').hide()
+}
+
+// 将方法写入插件,执行
+$('#item').menuAim({
+    activate: activeAni, // fired on row activation
+    deactivate: reactiveAni, // fired on row deactivation
+    exitMenu: function() { return true; }
+});
+
+// 顶部最大的轮播图效果
+$("#slideBoxSmall").tyslide({
+    boxh: 430, //盒子的高度
+    w: 1000, //盒子的宽度
+    h: 400, //图片的高度
+    isShow: true, //是否显示控制器
+    isShowBtn: true, //是否显示左右按钮
+    controltop: 10, //控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+    controlsW: 16, //控制按钮宽度
+    controlsH: 16, //控制按钮高度
+    radius: 8, //控制按钮圆角度数
+    controlsColor: "#d8d8d8", //普通控制按钮的颜色
+    controlsCurrentColor: "#ff6600", //当前控制按钮的颜色
+    isShowNumber: true,
+    marginNum: 2
+});
+
+
+// 为新书畅销榜,从你的全世界路过部分,添加相应的hover样式,因为还有一个地方有这个相同的样式,因此也拿到public里面来
+
+$('.wellSell').hover(function() {
+    // 因为出现了没有固定块的高度而导致每个书籍下方的横线出现变短的情况,因此在这里将每个盒子的高度固定
+    $('.wellSell').css('height', '42px')
+        // 将所有wellSell中的wellBook类隐藏
+    $('.wellSell').find('.wellBook').slideUp('fast')
+        // 将所有wellSell中的anitherText类隐藏
+    $('.wellSell').find('.anotherText').slideUp('fast')
+        // 将当前wellSell中的wellBook类显现
+    $(this).find('.wellBook').stop(true, false).slideDown('fast')
+        // 将当前wellSell中的anitherText类显现
+    $(this).find('.anotherText').stop(true, false).slideDown('fast')
+        // 设置当前wellSell的高度
+    $(this).css('height', '148px')
+}, function() {
+    // 经测试,使用hover效果,此处不放东西的效果比直接使用mouseover的效果要好,所以这里作为空数组保留
+})
+
+
+// 楼层的效果------------------------------------
+// 为左侧楼层增加相应的效果
+$('.leftFloor li').hover(function() {
+    // 为鼠标移入的span标签添加active
+    $(this).addClass('active')
+        // 定义一个leftFloorCurr,存储背景定位的X轴位置
+    var leftFloorCurr = parseInt($('.leftFloor .active').css('backgroundPositionX'))
+        // X轴位置交给本身
+    $(this).css('backgroundPositionX', leftFloorCurr - 40 + 'px')
+        // 为右侧内容设置滑动上去之后显示的效果
+    $(this).stop(true, false).animate({
+        width: '80px'
+    }, 100)
+}, function() {
+    //定义一个leftFloorCurr,存储背景定位的X轴位置
+    var leftFloorCurr = parseInt($('.leftFloor .active').css('backgroundPositionX'))
+        // X轴位置交给本身
+    $(this).css('backgroundPositionX', leftFloorCurr + 40 + 'px')
+        // 移除身上的active
+    $('.leftFloor li').removeClass('active')
+        // 为右侧内容设置滑动上去之后隐藏的效果
+    $(this).stop(true, true).animate({
+        width: '40px'
+    }, 100)
+})
+
+// 定义楼层的滚动效果
+$('.leftFloor li').on('click', function() {
+    var a = 1100;
+    $('html,body').animate({
+        scrollTop: a + $(this).index() * 610
+    }, 1000)
+})
+
+
+// 定义右侧楼策的效果,
+$('.rightFloor .discount').on('click', function() {
+    // 点击时,右侧的span移除
+    $(this).find('span').stop(true, false).animate({
+        right: '40px'
+    })
+}).on('mouseout', function() {
+    // 鼠标移出,则右侧的二维码隐藏
+    $(this).find('span').animate({
+        right: '-60px'
+    })
+})
+
+// 为二维码增加hover则暂时不隐藏的效果,并解决了动画一直在播放的bug
+$('.rightFloor .discount span').hover(function() {
+    $(this).stop(true, false).animate({
+        right: '40px'
+    })
+}, function() {
+    $(this).stop(true, true).animate({
+        right: '-60px'
+    })
+})
