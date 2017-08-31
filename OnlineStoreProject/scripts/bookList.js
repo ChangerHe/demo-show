eedb7c37bdcc10b1f0295d6fc0eabe10794e610c
ChangerHe <244737162@qq.com>
2017-08-31 15:58:57 +0800

    backup for start allbooksBanner

--- OnlineStoreProject/scripts/bookList.js
+++ OnlineStoreProject/scripts/bookList.js
@@ -26,3 +26,20 @@ $('.hotBooksRecommend').hover(function() {
     // 恢复之前的图片状态
     $(this).find('img').css('marginTop', '5px')
 })
+
+// 下方整体图书的轮播效果-----------------
+// $("#slideBooks").tyslide({
+//     boxh: 500, //盒子的高度
+//     w: 1200, //盒子的宽度
+//     h: 480, //图片的高度
+//     isShow: true, //是否显示控制器
+//     isShowBtn: true, //是否显示左右按钮
+//     controltop: 10, //控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+//     controlsW: 16, //控制按钮宽度
+//     controlsH: 16, //控制按钮高度
+//     radius: 8, //控制按钮圆角度数
+//     controlsColor: "#d8d8d8", //普通控制按钮的颜色
+//     controlsCurrentColor: "#ff6600", //当前控制按钮的颜色
+//     isShowNumber: true,
+//     marginNum: 2
+// });
