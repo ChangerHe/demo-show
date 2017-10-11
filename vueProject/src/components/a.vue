1d749f76740b1771f8d6be10de19ec61d8fbe2a3
ChangerHe <244737162@qq.com>
2017-10-11 10:20:06 +0800

    some changes

---
+++ vueProject/src/components/a.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-html="hello"></div>
+</template>
+<script>
+  export default {
+      data() {
+          return {
+              hello: 'I am component a'
+          }
+      }
+  }
+</script>
+
+<style>
+  * {
+    font-size: 20px;
+  }
+</style>
